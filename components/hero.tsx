d58db3552f6a8ddb5478025ec6bf96f499a4563f
Hoot Dev <72707472+error3236@users.noreply.github.com>
2024-10-11 18:45:00 +0530

    Changed Logo

--- components/hero.tsx
+++ components/hero.tsx
@@ -13,15 +13,15 @@ const Hero = () => {
     return (
         <section id='home'>
             <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-          
+
                 <div className="fixed top-0 left-10 mr-4">
-                <Image src="/hoot.png" alt='Hoot' height={90} width={90}/>
+                    <Image src="/hoot.png" alt='Hoot' height={90} width={90} />
                 </div>
-              
+
                 <BackgroundBeamsWithCollision>
-                <Image src="/hoot2.png" alt='Hum Studios' height={600} width={600}/>
-    </BackgroundBeamsWithCollision>
-               
+                    <Image src="/hoot.png" alt='Hum Studios' height={600} width={600} />
+                </BackgroundBeamsWithCollision>
+
                 {/* <a href="#featured" className=""><ShimmerButton title="Let's Go!!" position="left" /> </a> */}
             </div>
         </section>
